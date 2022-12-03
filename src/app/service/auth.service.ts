ce2fd41aa3fc107447fc215b9a6a81c24aaf64a5
nayan-suhagiya <nayan.suhagiya01@gmail.com>
2022-12-03 17:12:41 +0530

    logged in to firebase

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { loginData } from '../interface/login';
 import { registerData } from '../interface/register';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { NgxSpinnerService } from 'ngx-spinner';
 import Swal from 'sweetalert2';
 
@@ -13,7 +14,21 @@ export class AuthService {
   constructor(private spinner: NgxSpinnerService, private router: Router) {}
 
   loginSubmit(data: loginData) {
-    console.log(data);
+    // console.log(data);
+    this.spinner.show();
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, data.email, data.passwd)
+      .then((userCredential) => {
+        // const user = userCredential.user;
+        // console.log('Logged in');
+        this.spinner.hide();
+        Swal.fire('Success!', 'User LoggedIn SuccessFully!', 'success');
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        Swal.fire('Error!', 'Email And Password Not Matched!', 'error');
+      });
   }
 
   registerSubmit(data: registerData) {
@@ -22,17 +37,17 @@ export class AuthService {
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, data.email, data.passwd)
       .then((userCredential) => {
-        const user = userCredential.user;
-        console.log('User added');
+        // const user = userCredential.user;
+        // console.log('User added');
         this.spinner.hide();
         this.router.navigate(['/login']);
         Swal.fire('Success!', 'User Added SuccessFully!', 'success');
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log('error');
-        Swal.fire('Error!', 'Sme Error Occurs!', 'error');
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // console.log('error');
+        Swal.fire('Error!', 'Please Provide Valid Information!', 'error');
       });
   }
 }
